38a1ae1e9d4c8e524f6bc5f60d62bd62687283a8
zjgwh <76939221@qq.com>
2016-11-19 13:14:16 +0800

    1

---
+++ C语言程序设计项目化教程王彩霞主编/002C的基本数据类型以及有关运算/2p25.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+
+printf("中国北京于2008年8月8日举办了第20届奥林匹克运动会\n");
+printf("中国北京于%d年%d月%d日举办了第%d届奥林匹克运动会\n",2008,8,8,20);
+
+/*
+int x;
+char name[40],y;
+float z;
+printf("请输入:\n(提示一次性输入整数,串,小数 ,单字符)\n");
+scanf("%d%s%f%c",&x,name,&z,&y);   //一次性输入整数,串,小数 ,单字符
+printf("x=%d,name=%s,y=%c,z=%.2f\n",x,name,y,z);
+*/
+  
+	}
